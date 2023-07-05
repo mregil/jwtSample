d27b1f8d2a7b4a098e0c6f541d8c7e179999fe51
Mario Regil <mregil@ymmc.yamaha-motor.com>
2023-07-05 12:20:00 +0000

    Initial

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -17,14 +17,14 @@ var config = require('../config'); // get config file
 
 router.post('/login', function(req, res) {
 
-  User.findOne({ email: req.body.email }, function (err, user) {
-    if (err) return res.status(500).send('Error on the server.');
+  User.findOne({ email: req.body.email } ).then ( (user) => {
     if (!user) return res.status(404).send('No user found.');
-    
-    // check if the password is valid
     var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
     if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
 
+  }).catch((err) => {
+    if (err) return res.status(500).send('Error on the server.');
+    if (!user) return res.status(404).send('No user found.');
     // if user is found and password is valid
     // create a token
     var token = jwt.sign({ id: user._id }, config.secret, {
@@ -33,9 +33,8 @@ router.post('/login', function(req, res) {
 
     // return the information including token as JSON
     res.status(200).send({ auth: true, token: token });
-  });
-
-});
+  })});
+  
 
 router.get('/logout', function(req, res) {
   res.status(200).send({ auth: false, token: null });
@@ -49,29 +48,29 @@ router.post('/register', function(req, res) {
     name : req.body.name,
     email : req.body.email,
     password : hashedPassword
-  }, 
-  function (err, user) {
-    if (err) return res.status(500).send("There was a problem registering the user`.");
-
+  }).then((user) => {
     // if user is registered without errors
     // create a token
     var token = jwt.sign({ id: user._id }, config.secret, {
       expiresIn: 86400 // expires in 24 hours
     });
-
-    res.status(200).send({ auth: true, token: token });
+    return res.status(200).send({ auth: true, token: token });
+ }).catch((err) => {
+   return res.status(500).send("There was a problem registering the user`." + err);
   });
 
 });
 
 router.get('/me', VerifyToken, function(req, res, next) {
-
-  User.findById(req.userId, { password: 0 }, function (err, user) {
-    if (err) return res.status(500).send("There was a problem finding the user.");
+console.log("looking up me with token");
+  User.findById(req.userId,{ password: 0 }).then( (user) => {
     if (!user) return res.status(404).send("No user found.");
     res.status(200).send(user);
-  });
+    next();
+  } ).catch((err) => {
+    if (err) return res.status(500).send("There was a problem finding the user.");
 
-});
+  })});
 
+  
 module.exports = router;
--- user/UserController.js
+++ user/UserController.js
@@ -9,27 +9,27 @@ var User = require('./User');
 
 // CREATES A NEW USER
 router.post('/', function (req, res) {
-    User.create({
+	User.create({
             name : req.body.name,
             email : req.body.email,
             password : req.body.password
-        }, 
-        function (err, user) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(user);
-        });
+        }).then((result) => {
+  		return res.status(200).send(result)
+	}).catch((err) => {
+  		return res.status(500).send("there was a problem adding info to the DB")
+        })
 });
 
 // RETURNS ALL THE USERS IN THE DATABASE
-router.get('/', function (req, res) {
-    User.find({}, function (err, users) {
-        if (err) return res.status(500).send("There was a problem finding the users.");
-        res.status(200).send(users);
-    });
+router.get('/',  VerifyToken, function (req, res) {
+    User.find({}).then((users) =>  {
+            res.status(200).send(users) 
+           }).catch((err) => {return res.status(500).send("There was a problem finding the users.")
+   });
 });
 
 // GETS A SINGLE USER FROM THE DATABASE
-router.get('/:id', function (req, res) {
+router.get('/:id',  VerifyToken, function (req, res) {
     User.findById(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
@@ -38,7 +38,7 @@ router.get('/:id', function (req, res) {
 });
 
 // DELETES A USER FROM THE DATABASE
-router.delete('/:id', function (req, res) {
+router.delete('/:id',  VerifyToken,function (req, res) {
     User.findByIdAndRemove(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem deleting the user.");
         res.status(200).send("User: "+ user.name +" was deleted.");
@@ -47,7 +47,7 @@ router.delete('/:id', function (req, res) {
 
 // UPDATES A SINGLE USER IN THE DATABASE
 // Added VerifyToken middleware to make sure only an authenticated user can put to this route
-router.put('/:id', /* VerifyToken, */ function (req, res) {
+router.put('/:id',  VerifyToken,  function (req, res) {
     User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(user);
